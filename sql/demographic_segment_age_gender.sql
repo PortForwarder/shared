0390e1e533fd378dc51b092ff27d82447a86b396
PortForwarder <61743001+PortForwarder@users.noreply.github.com>
2022-02-01 08:56:23 +0900

    Update demographic_segment_age_gender.sql

--- sql/demographic_segment_age_gender.sql
+++ sql/demographic_segment_age_gender.sql
@@ -40,6 +40,9 @@ when (age <= 44) and (gender = 'female') then '11:女性35-44歳'
 when (age <= 54) and (gender = 'female') then '12:女性45-54歳'
 when (age <= 64) and (gender = 'female') then '13:女性55-64歳'
 when (age >= 65) and (gender = 'female') then '14:女性65歳以上'
+  --Queer(LGBTQ+)
+  --Unrevealed
+
 
 
 --StandardMarketingFrame(age+gender)
@@ -57,7 +60,9 @@ when (age <= 49) and (gender = 'female') then '11:女性40代'
 when (age <= 59) and (gender = 'female') then '12:女性50代'
 when (age <= 69) and (gender = 'female') then '13:女性60代'
 when (age >= 70) and (gender = 'female') then '14:女性70歳以上'
-
+  --Queer(LGBTQ+)
+  --Unrevealed
+  
 
 --StandardMarketingFrame(age+gender)
 when (age <= 17) and (gender = 'male') then '01:男性17歳以下'
@@ -74,7 +79,9 @@ when (age <= 44) and (gender = 'female') then '11:女性35-44歳'
 when (age <= 54) and (gender = 'female') then '12:女性45-54歳'
 when (age <= 64) and (gender = 'female') then '13:女性55-64歳'
 when (age >= 65) and (gender = 'female') then '14:女性65歳以上'
-
+  --Queer(LGBTQ+)
+  --Unrevealed
+  
 
 --StandardMarketingFrame(FMCT)
 when (age <= 12) and (gender = 'male') then '01:C層(男女12歳以下)'
@@ -89,7 +96,8 @@ when (age <= 34) and (gender = 'female') then '07:F1層(女性20-34歳)'
 when (age <= 49) and (gender = 'female') then '08:F2層(女性35-49歳)'
 when (age <= 64) and (gender = 'female') then '09:F3層(女性50-64歳)'
 when (age >= 65) and (gender = 'female') then '10:F4層(女性65歳以上)'
-
-
+  --Queer(LGBTQ+)
+  --Unrevealed
+  
 --else '' end as your_segment_name
 
