8b78351f9c7b0907d990f636787ec4580e82bd05
kei <61743001+PortForwarder@users.noreply.github.com>
2022-09-09 17:26:44 +0900

    Update weekday.sql

--- sql/weekday.sql
+++ sql/weekday.sql
@@ -1,9 +1,8 @@
 SELECT CASE
+  when dow(cast(time as timestamp)) = 0 then '日'
   when dow(cast(time as timestamp)) = 1 then '月'
-  when dow(cast(time as timestamp)) = 1 then '火'
-  when dow(cast(time as timestamp)) = 1 then '水'
-  when dow(cast(time as timestamp)) = 1 then '木'
-  when dow(cast(time as timestamp)) = 1 then '金'
-  when dow(cast(time as timestamp)) = 1 then '土'
-  when dow(cast(time as timestamp)) = 1 then '日'
-  else '' end as weekday
+  when dow(cast(time as timestamp)) = 2 then '火'
+  when dow(cast(time as timestamp)) = 3 then '水'
+  when dow(cast(time as timestamp)) = 4 then '木'
+  when dow(cast(time as timestamp)) = 5 then '金'
+  when dow(cast(time as timestamp)) = 6 then '土'
